49e38c432aaf797534fc515290ded7a1a4f85825
merepapa <82329144+merepapa@users.noreply.github.com>
2024-12-08 13:37:42 +0530

    Final Update with working application

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:8000"], // Update this to match your frontend port
+    origin: ["http://localhost:3000"], // Update this to match your frontend port
     methods: ["GET", "POST"],
     credentials: true,
   },
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,6 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (authUser) {
       const socket = io("http://localhost:5000", {
-        // or whatever port your backend is running on
         query: {
           userId: authUser._id,
         },
